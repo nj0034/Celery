1658de1e8f536f02172bada514cbe742883118e3
namjin lee <nj@devhi.me>
2018-06-18 14:28:52 +0900

    activity thumbnail 추가

--- luna_celery/celery.py
+++ luna_celery/celery.py
@@ -20,7 +20,17 @@ app.autodiscover_tasks()
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
+    sender.add_periodic_task(crontab(), test.s('world'))
 
+
+app.conf.beat_schedule = {
+    'add-every-10-seconds': {
+        'task': 'test',
+        'schedule': 10.0,
+        'args': ('test', 'success')
+    }
+}
+app.conf.timezone = 'UTC'
 
 @app.task
 def test(arg):
